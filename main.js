66a91b3b1c4222572ef1f93c34a945d633f77e70
Caue Campos <cauewc05@gmail.com>
2024-11-18 00:10:58 -0300

    Update files.

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function outcome () {
+
+    let firstNum = Number(document.getElementById("num-one").value)
+    let secNum = Number(document.getElementById("num-two").value)
+    let total = 0
+
+    if(document.getElementById('box1').checked)
+        total = firstNum + secNum
+    else if(document.getElementById('box2').checked)
+        total = firstNum - secNum
+    else if(document.getElementById('box3').checked)
+        total = firstNum * secNum
+    else
+        total = firstNum / secNum
+    document.getElementById('resultArea').innerHTML = 'Resultado: ' + String(total)
+}
